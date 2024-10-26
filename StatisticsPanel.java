3e97f0aaeb8987d5f4ec3c9823c7b5948bc5e66f
Aida Tadlaoui <153181540+aida-tdl@users.noreply.github.com>
2024-10-26 20:58:24 +0100

    Update StatisticsPanel.java

--- StatisticsPanel.java
+++ StatisticsPanel.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
+ * @author Aïda Tadlaoui, Saruta Kittipattananon, Chen Wang and Isabella Landgrebe
  * The StatisticsPanel class extends BorderPane to create a UI component that displays various COVID-19 related statistics.
  * It allows navigating through different statistics using navigation buttons.
  */
